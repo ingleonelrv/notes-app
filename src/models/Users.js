17d9e6e7e4bcb05124b3639dbc7f942685cb16e8
ingleonelrv <ing.leonelrv@gmail.com>
2019-02-14 18:21:14 -0600

    Signup

---
+++ src/models/Users.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose')
+const bcrypt=require('bcryptjs')
+const {Schema}=mongoose
+
+const UserSchema=new Schema({
+    name:{type:String, required:true},
+    email:{type:String, required:true},
+    password:{type:String, required:true},
+    date:{type:Date, default:Date.now}
+})
+//creo metodo para encriptar datos usando el modulo bcrypt
+UserSchema.methods.encryptPassword=async (password)=>{
+    //metodo q genera un hash, le puedo dar mas ciclos
+    const salt=await bcrypt.genSalt(10)
+    //aplico el hash al password
+    const hash=bcrypt.hash(password,salt)
+    return hash
+}
+//tomara la constrasenia signin y lo comparara con lo q este en la DB
+UserSchema.methods.matchPassword= async function(password){
+    //uso function y no arrow para poder usar this
+    return await bcrypt.compare(password,this.password)
+}
+
+module.exports=mongoose.model('User',UserSchema)
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,8 @@
 //URL's DE AUTENTICACION:/login /signin
 //ocupo express no para crear server sino para crear rutas
 const router=require('express').Router()
+//instancio el modelo User para crear nuevo User
+const User =require('../models/Users')
 
 //cuando salte el metodo get, segun ruta, mostrara algo
 router.get('/users/signin',(req,res)=>{
@@ -8,5 +10,42 @@ router.get('/users/signin',(req,res)=>{
 })
 router.get('/users/signup',(req,res)=>{
     res.render('users/signup')
+})
+//cuando de click en Register traera aca, se llama igual al GET pero como este es POST no problem
+router.post('/users/signup',async (req,res)=>{
+    //desestructuro los datos q me llegan en request
+    const {name, email,password,confirmPassword}=req.body
+    //creo una const para almacenar errores
+    const errors=[]
+    //validaciones
+    if(name.length<=0||email.length<=0){
+        errors.push({text:'Please fill all fields'})
+    }
+    if(password!=confirmPassword){
+        errors.push({text:'Password do not match!'})
+    }
+    if(password.length<6){
+        errors.push({text:'Password must be at least 6 characters'})
+    }
+    //si lanzo algun error direcciono de nuevo al formulario pero con los datos y ademas el error
+    if(errors.length>0){
+        res.render('users/signup',{errors,name,email,password,confirmPassword})
+    }else{
+        //valido que no me este dando un correo q ya existe
+        const emailUser=await User.findOne({email:email})
+        if(emailUser){
+            req.flash('error_msg','The email is already in use')
+            res.redirect('/users/signup')
+        }
+        //creo un nuevo usuario(objeto) usando el schema
+        const newUser=new User({name,email,password})
+        //luego cifro la constrasenia llamando al metodo y reemplazo el dato del objeto newUser
+        newUser.password=await newUser.encryptPassword(password)
+        //ahora lo guardo en la bd
+        await newUser.save()
+        //luego de guardar envio un msg flash
+        req.flash('success_msg','You are registered now')
+        res.redirect('/users/signin')
+    }
 })
 module.exports=router
